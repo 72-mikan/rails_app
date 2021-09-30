91cb89af86c87fe80ad14b67924cb5d252ab4255
72_mikan_desktop <shinzi7280.18@gmail.com>
2021-09-30 14:10:15 +0900

    rails_app3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e7e153c95aeb46e400338a3019063acb0abfa5217d8801c366c508d05c1a3bf2333504a54951502edbc05d68ac42ac032ef72acfc17aac571e5ecbfc94f67bac'
