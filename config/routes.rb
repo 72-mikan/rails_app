18d8a28d39a583eb7a841cf6bc7ea40bd519887d
72_mikan_desktop <shinzi7280.18@gmail.com>
2021-10-06 16:24:10 +0900

    rails_app5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'user/show'
   root to: 'homes#top'
   devise_for :users
@@ -6,6 +7,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 
 end
